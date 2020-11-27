5ea53345463ae041454fd9c9911aa79d86ded6c9
Andrew Barnett <Abarn279@gmail.com>
2020-11-27 15:00:15 -0600

    9

---
+++ src/_9b.py
@@ -0,0 +1,44 @@
+from file_importer import FileImporter
+from collections import deque
+import sys
+
+distances = FileImporter.get_input("/../input/9.txt").split('\n')
+distance_map = {}
+
+for d in distances:
+    d = d.split(' ')
+    distance_map[(d[0], d[2])] = int(d[4])
+    distance_map[(d[2], d[0])] = int(d[4])
+
+all_cities = set()
+for k in distance_map.keys():
+    for i in k:
+        all_cities.add(i)
+
+stack = deque()
+visited = set()
+
+for ac in all_cities:
+    stack.appendleft(((ac,), 0)) # tuple of cities visited, current distance
+
+c_best = -1
+
+while len(stack) > 0:
+    [cities, dist] = stack.popleft()
+
+    # pruning for if we can't possibly get another best
+    current_city_set = set(cities) 
+
+    if current_city_set == all_cities and dist > c_best:
+        c_best = dist
+        continue
+
+    # get set of remaining cities to go to 
+    remaining_city_set = all_cities - current_city_set
+
+    for remaining in remaining_city_set:
+        new_path = cities + (remaining,)
+        new_dist = dist + distance_map[(cities[-1], remaining)]
+        stack.appendleft((new_path, new_dist))
+
+print(c_best)
